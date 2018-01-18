2aff9788db141f313c2dd081260714a95ac7f439
Richard Eakin <rtepub@gmail.com>
2018-01-17 23:04:53 -0500

    improve velocity debug render

--- samples/_opengl/MotionBlurVelocityBuffer/assets/velocity-render.fs
+++ samples/_opengl/MotionBlurVelocityBuffer/assets/velocity-render.fs
@@ -9,8 +9,22 @@ out vec4 fColor;
 
 void main()
 {
-  vec4 color = vec4(1.0);
-  color.rg = texture( uTex0, vTexCoord ).rg * 255.0 + vec2(0.5);
+  vec2 vel = texture( uTex0, vTexCoord ).xy;
+
+  const float velScale = 8.0;
+  const float velThreshold = 0.01;
+
+  vec4 color = vec4( 0.08, 0.08, 0.08, 1.0 );
+  
+  if( vel.x > velThreshold )
+  	color.r += vel.x * velScale;
+  else if( vel.x < - velThreshold )
+  	color.g += - vel.x * velScale;
+  
+  if( vel.y > velThreshold )
+  	color.b += vel.y * velScale;
+  else if( vel.y < - velThreshold )
+  	color.rg += - vel.y * velScale;  	
 
   fColor = color;
 }
