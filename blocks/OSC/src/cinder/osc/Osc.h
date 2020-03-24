b76eabb3d4bccd6308438c00d19ba990b780aa7d
Rich E <rich.eakin@gmail.com>
2020-03-24 18:55:51 -0400

    osc: added methods for removing all listeners and getting all listeners.

--- blocks/OSC/src/cinder/osc/Osc.h
+++ blocks/OSC/src/cinder/osc/Osc.h
@@ -647,7 +647,11 @@ public:
 	void		setListener( const std::string &address, ListenerFn listener );
 	//! Removes the listener associated with \a address.
 	void		removeListener( const std::string &address );
-	
+	//! Removes all listeners
+	void		removeAllListeners();
+	//! Returns all listeners by value
+	Listeners getListeners() const;
+
   protected:
 	ReceiverBase() = default;
 	//! Non-copyable.
@@ -676,7 +680,7 @@ public:
 	virtual void closeImpl() = 0;
 	
 	Listeners				mListeners;
-	std::mutex				mListenerMutex;
+	mutable std::mutex		mListenerMutex;
 	std::set<std::string>	mDisregardedAddresses;
 };
 	
